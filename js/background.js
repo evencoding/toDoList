652345bf95091cfe1aae04d41fab9ce4593c8330
evencoding <88191233+evencoding@users.noreply.github.com>
2021-08-06 05:26:01 +0900

    # version 2
    경로 못찾는 에러 수정

--- js/background.js
+++ js/background.js
@@ -4,6 +4,6 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
-bgImage.src = `src/${chosenImage}`;
+bgImage.src = `./src/${chosenImage}`;
 
 document.body.appendChild(bgImage);
